5c3f6dc29d2bd40877122375c99f80c8bf8ae4f7
Pepijn Nichting <pepijn.nichting@gmail.com>
2025-02-14 17:48:21 +0100

    Refactor CAN communication parameters and improve speed handling in BafangSpeedAdjust_esp32.ino

--- BafangSpeedAdjust/BafangSpeedAdjust_esp32.ino
+++ BafangSpeedAdjust/BafangSpeedAdjust_esp32.ino
@@ -2,11 +2,15 @@
 #include <CAN.h>
 
 bool speedSet = false;
+const long canBaudRate = 250E3;
+const long esp32BaudRate = 115200;
+const int speedKmH = 35; // Speed in km/h
+const long canId = 0x85103203; // CAN ID
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(esp32BaudRate);
   
-  if (!CAN.begin(250E3)) {
+  if (!CAN.begin(canBaudRate)) {
     Serial.println("Starting CAN failed!");
     while (1);
   }
@@ -17,10 +21,11 @@ void setup() {
   //Original read: 82F83203 6 C4 09 C0 2B CE 08 => C4 09 => 09C4 => 2500 => 25km/h
   //Set:           85103203 6 AC 0D C0 2B CE 08 => AC 0D => 0DAC => 3500 => 35km/h
   //New read:      82F83203 6 AC 0D C0 2B CE 08
+  int speed = speedKmH * 100; // Convert km/h to the required format
   
-  CAN.beginExtendedPacket(0x85103203);
-  CAN.write(0xAC);
-  CAN.write(0x0D);
+  CAN.beginExtendedPacket(canId);
+  CAN.write((speed >> 8) & 0xFF); // High byte
+  CAN.write(speed & 0xFF);        // Low byte
   CAN.write(0xC0);
   CAN.write(0x2B);
   CAN.write(0xCE);
@@ -61,9 +66,11 @@ void loop() {
     Serial.println("------ WRITING SPEED ------");
     Serial.println("------ WRITING SPEED ------");
 
-    CAN.beginExtendedPacket(0x85103203);
-    CAN.write(0xAC);
-    CAN.write(0x0D);
+    int speed = speedKmH * 100; // Convert km/h to the required format
+
+    CAN.beginExtendedPacket(canId);
+    CAN.write((speed >> 8) & 0xFF); // High byte
+    CAN.write(speed & 0xFF);        // Low byte
     CAN.write(0xC0);
     CAN.write(0x2B);
     CAN.write(0xCE);
